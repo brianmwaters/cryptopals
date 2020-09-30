322fe082c7fd2dc4a258e3965ec06c3d4317202c
Brian Waters <brian@brianmwaters.net>
2020-09-29 21:46:13 -0400

    Fix Set 1, Challenge 7
    Our AES-ECB implementation should not do PKCS #7 padding.

--- cryptopals/challenge7.py
+++ cryptopals/challenge7.py
@@ -1,13 +1,9 @@
 """Solution to Cryptopals Set 1, Challenge 7."""
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives.padding import PKCS7
 
 
 def aes_ecb_decrypt(key, ct):
-    """Decrypt a message encrypted with AES-ECB and PKCS #7 padding."""
+    """Decrypt a message encrypted with AES-ECB."""
     decryptor = Cipher(algorithms.AES(key), modes.ECB()).decryptor()
-    unpadder = PKCS7(len(key) * 8).unpadder()
-    padded = decryptor.update(ct) + decryptor.finalize()
-    pt = unpadder.update(padded) + unpadder.finalize()
-    return pt
+    return decryptor.update(ct) + decryptor.finalize()
